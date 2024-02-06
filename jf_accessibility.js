88b6df805145ff7e7dfc2836af37ddd1d07d3aec
jasitu-perkins <120684662+jasitu-perkins@users.noreply.github.com>
2024-02-06 13:10:05 -0500

    removed footer/main conditional

---
+++ jf_accessibility.js
@@ -0,0 +1,62 @@
+// Get all form elements inside the main with the class 'jotform-form'
+var forms = document.querySelector('main').getElementsByClassName('jotform-form');
+
+// If class 'jotform-form' also has class 'redirect', continue. Otherwise, return
+for (var i = 0; i < forms.length; i++) {
+   if (!forms[i].classList.contains('redirect')) {
+       continue;
+   }
+}
+
+// Get all the submit buttons
+var submitButtons = document.querySelectorAll('.form-submit-button');
+
+// Store the original textContent of the buttons
+var originalTextContents = Array.from(submitButtons).map(button => button.textContent);
+
+// Loop over each form
+Array.from(forms).forEach(function(form, formIndex) {
+  // Create a new iframe element
+  var iframe = document.createElement('iframe');
+
+  // Set the iframe's properties
+  iframe.name = 'jf_iframe_' + form.id;
+  iframe.id = 'jf_iframe_' + form.id;
+  iframe.style.display = 'none';
+  iframe.className = 'jf_iframe'; // Add a class to the iframe
+
+  // Append the iframe directly after the form
+  form.insertAdjacentElement('afterend', iframe);
+
+  // Set the form's target to the ID of the iframe
+  form.target = iframe.id;
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // Get the form and iframe elements
+    var formContainer = this; // 'this' refers to the form being submitted
+    var iframe = document.getElementById('jf_iframe_' + this.id);
+
+    // Listen for the iframe to load
+    iframe.addEventListener('load', function() {
+      // Once the iframe is loaded, hide the form and show the iframe
+      formContainer.style.display = 'none';
+      iframe.style.display = 'block';
+
+      // Change the text of all buttons back to their original state
+      submitButtons.forEach((button, index) => {
+        button.textContent = originalTextContents[index];
+        
+        // Re-enable the button if it was previously disabled
+        if (button.classList.contains('lastDisabled')) {
+          button.classList.remove('lastDisabled');
+          button.disabled = false;
+        }
+      });
+    });
+
+    // Continue with the form submission
+    this.submit();
+  });
+});
